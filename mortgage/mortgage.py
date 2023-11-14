5b2acb7fb752efabd36cb061409c654bd4325ca3
Simrankaur2007 <kaursimran5315@gmail.com>
2023-11-14 21:53:40 +0530

    pushed validate_loan_amount function

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -1,9 +1,30 @@
 """
 Description: A class meant to manage Mortgage options.
-Author: {Student Name}
-Date: {Date}
+Author: Simran Kaur
+Date: 13/11/2023
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
 
+# mortgage.py
 
+from mortgage.pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
+
+class Mortgage:
+    def __init__(self, loan_amount, rate, frequency, amortization):
+        self.loan_amount = loan_amount
+        self.rate = rate
+        self.frequency = frequency
+        self.amortization = amortization
+
+        # Validate inputs
+        self._validate_loan_amount()
+        self._validate_rate()
+        self._validate_frequency()
+        self._validate_amortization()
+
+    def _validate_loan_amount(self):
+        if self.loan_amount <= 0:
+            raise ValueError("Loan Amount must be positive.")
+
+    
--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -1,7 +1,43 @@
 """
 Description: A class used to test the Mortgage class.
-Author: {Student Name}
-Date: {Date}
+Author: Simran Kaur
+Date: 13/11/2023
 Usage: Use the tests encapsulated within this class to test the MortgagePayment class.
 """
 
+# test_mortgage.py
+
+import unittest
+from mortgage.mortgage import Mortgage
+from mortgage.pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
+
+class MortgageTests(unittest.TestCase):
+    
+    def test_init_valid_inputs(self):
+        loan_amount = 500000
+        rate = MortgageRate.FIXED_5
+        frequency = MortgageFrequency.MONTHLY
+        amortization = 30
+
+        mortgage = Mortgage(loan_amount, rate, frequency, amortization)
+
+        # Verify using accessors
+        self.assertEqual(mortgage.loan_amount, loan_amount)
+        self.assertEqual(mortgage.rate, rate)
+        self.assertEqual(mortgage.frequency, frequency)
+        self.assertEqual(mortgage.amortization, amortization)
+ 
+    def test_loan_amount_accessor_mutator(self):
+        mortgage = Mortgage(500000, MortgageRate.FIXED_5, MortgageFrequency.MONTHLY, 30)
+        
+        # Test Mutator (set to invalid value)
+        with self.assertRaises(ValueError):
+            mortgage.loan_amount = -100000
+
+        # Test Mutator (set to zero)
+        with self.assertRaises(ValueError):
+            mortgage.loan_amount = 0
+
+        # Test Mutator (set to positive value)
+        mortgage.loan_amount = 600000
+        self.assertEqual(mortgage.loan_amount, 600000)
